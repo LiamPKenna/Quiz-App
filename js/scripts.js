7b204e276ec386b3462de0abcfb3ba1dddbc3ae3
LiamPKenna <liampkenna@gmail.com>
2019-10-25 09:58:27 -0700

    add mock up on initial question card

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// const cardBuilder = function(questionIndex) {
+//   const thisQuestion = questions[questionIndex];
+//   if (thisQuestion.answer) {
+//     return answerBuilder(thisQuestion);
+//   } else if (thisQuestion.answerFunction){
+//     return ``;
+//   } else {
+//     return `
+//     <h3>${thisQuestion.question}</h3>
+//     <br><br>
+//
+//     `
+//   }
+// }
+
+
+
+// $(document).ready(function() {
+//   $("question").
+// });
