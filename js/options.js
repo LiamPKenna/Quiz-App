b6f9d7a2019b656455670904662711ea97dda91d
LiamPKenna <liampkenna@gmail.com>
2019-10-27 05:29:32 -0700

    add options object in options.js

---
+++ js/options.js
@@ -0,0 +1,12 @@
+const options = [
+  {
+    name: "language",
+    buttonText: "Find your Programming Language",
+    scriptSrc: "js/language.js"
+  },
+  {
+    name: "career",
+    buttonText: "Find your Career",
+    scriptSrc: "js/language.js"
+  }
+];
